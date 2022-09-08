5e6f49260d60213dbc3b7ae5fad539c9498eabef
sout_rahim <sout.rahim@gmail.com>
2022-09-08 22:17:16 +0700

    update setting

--- lib/app/home/tab_item.dart
+++ lib/app/home/tab_item.dart
@@ -11,6 +11,6 @@ class TabItemData {
     TabItem.streams: TabItemData(title: 'Stream', icon: Icons.videocam),
     TabItem.schedules: TabItemData(title: 'Schedule', icon: Icons.timelapse),
     TabItem.quran: TabItemData(title: 'Quran', icon: Icons.auto_stories_rounded),
-    TabItem.account: TabItemData(title: 'Account', icon: Icons.person),
+    TabItem.account: TabItemData(title: 'Setting', icon: Icons.settings),
   };
 }
